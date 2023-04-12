b448202b3ac7d3f154d21879f50c31ae2f7b501c
ahmed taha <bjabotaha@yahoo.com>
2023-04-12 11:14:03 +0200

    problem 1 edit

--- problems/1_Combine_Nodes_Between_Zeros.cpp
+++ problems/1_Combine_Nodes_Between_Zeros.cpp
@@ -1,62 +1,88 @@
-#include "../implementations/3- Doubly Linked List/doublyLinkedList.cpp"
+/*this program merges numbers between zeros and add it and remove the zeros*/
+// made by : ahmed mohamed taha
+// id : 20210033
 
-void solve( doublyLinkedList<int>& ls ){
+#include <bits\stdc++.h>
+using namespace std;
 
-    int size = ls.doubleLinkedListSize(), i = 1, sum = 0;
-    ls.removeAtHead();
-    while (i < size){
+struct node{
+    int val;
+    node* next;
 
-        sum += ls.retriveAt(0);
-        i++;
-        if(ls.retriveAt(0) == 0){
-            ls.insertAtTail(sum);
-            sum = 0;
-        }
-        ls.removeAtHead();
+    node(){
+        val = 0;
+        next = nullptr;
     }
+};
 
+void print_ls(node* n){
+    if(n == nullptr)
+        return;
+
+    cout << n->val << " ";
+    print_ls(n->next);
 }
 
-int main(){
-    doublyLinkedList<int> ls;
+void solve(node*& head){
+    int size = 1, i = 1, sum = 0;
+    node* curr = head;
 
-    // first test [0, 3, 1, 0, 4, 5, 2, 0]
-    cout << "first test's ";
-    ls.insertAtTail(0);
-    ls.insertAtTail(3);
-    ls.insertAtTail(1);
-    ls.insertAtTail(0);
-    ls.insertAtTail(4);
-    ls.insertAtTail(5);
-    ls.insertAtTail(2);
-    ls.insertAtTail(0);
-    cout << "list before solve : ";
-    ls.forwardTraversal();
-
-
-    solve(ls);
-    cout << "list after solve : ";
-    ls.forwardTraversal();
+    while (curr -> next != nullptr){
+        curr = curr -> next;
+        size++;
+    }
     
+    node* tempp = head;
+    head = head -> next;
+    delete tempp;
 
-    ls.clear();
+    while (i < size){
+
+        sum += head -> val;
+        i++;
+        if(head -> val == 0){
+            node* temppp = new node();
+            temppp -> val = sum;
+            curr -> next = temppp;
+            curr = curr -> next;
+            sum = 0;
+        }
+
+        node* temp = head;
+        head = head -> next;
+        delete temp;
+    }
+}
 
 
-    // second test [0, 1, 0, 3, 0, 2, 2, 0]
-    cout << "second test's ";
-    ls.insertAtTail(0);
-    ls.insertAtTail(1);
-    ls.insertAtTail(0);
-    ls.insertAtTail(3);
-    ls.insertAtTail(0);
-    ls.insertAtTail(2);
-    ls.insertAtTail(2);
-    ls.insertAtTail(0);
-    cout << "list before solve : ";
-    ls.forwardTraversal();
+/* tests */
+// 0 3 1 0 4 5 2 0
+// 0 1 0 3 0 2 2 0
 
+int main(){
+    node* head = nullptr;
+    node* curr = nullptr;
+    int n;
+    cout << "please enter # of values : ";   cin >> n;
+    while (n--){
+        int num;    cout << "please enter number : "; cin >> num;
+        node* temp = new node();
+        temp->val = num;
+        if(head == nullptr){
+            head = temp;
+            curr = head;
+            continue;
+        }
+        curr->next = temp;
+        curr = curr -> next;
+    }
+    
+    cout << "values before solve : ";
+    print_ls(head);
+    
+    solve(head);
+    cout << head->val;
+    cout << "\nlist after solve : ";
+    print_ls(head);
 
-    solve(ls);
-    cout << "list after solve : ";
-    ls.forwardTraversal();
 }
